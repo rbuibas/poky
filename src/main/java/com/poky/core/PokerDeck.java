ef9d6ee4fbff5366611171798e7a2c663ab92794
rbuibas <buibas.raul@gmail.com>
2020-03-27 17:50:10 +0100

    Implement new methods in PokerDeck + comments

--- src/main/java/com/poky/core/PokerDeck.java
+++ src/main/java/com/poky/core/PokerDeck.java
@@ -5,74 +5,173 @@ import com.poky.core.PokerCardProperties.Value;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.poky.util.DeckUtils.deckToPrettyString;
 
 /**
  * The playing deck
  */
 
 public class PokerDeck implements Deck<Card> {
-    private List<Card> cards;
-    // how many cards are in deck at a certain moment
-    private int cardsInDeck;
-    Logger log;
 
-    private static final int CARDS_IN_STARTING_DECK = 52;
+    protected final Logger log = LogManager.getLogger(this.getClass());
+    /**
+     * Maximum number of cards in deck.
+     */
+    int CARDS_IN_STARTING_DECK = 52;
+    /**
+     * Upper limit: head's up game with "run it 6 times". Do the math.
+     */
+    int MAX_BURNT_CARDS = 18;
+    /**
+     * Upper limit: 10 player game with "run it 4 times". Do the math.
+     */
+    int MAX_DEALT_CARDS = 40;
+    /**
+     * The collection holding the live cards.
+     * All the cards are live cards at the moment of deck initialization.
+     */
+    private List<Card> liveCards;
+    private List<Card> burntCards;
+    private List<Card> dealtCards;
+    /**
+     * Flag blocking the shuffling of the deck.
+     * For example: if a game gas started shuffling should not be permitted.
+     */
+    private boolean shuffleBlocked;
 
     public PokerDeck() {
-        log = LogManager.getLogger(this.getClass());
-        cards = new ArrayList<>(CARDS_IN_STARTING_DECK);
-        resetDeck();
+        liveCards = new ArrayList<>(CARDS_IN_STARTING_DECK);
+        burntCards = new ArrayList<>(MAX_BURNT_CARDS);
+        dealtCards = new ArrayList<>(MAX_DEALT_CARDS);
+        initialize();
     }
 
+    /**
+     * Sets up the deck by creating {@link #CARDS_IN_STARTING_DECK} cards,
+     * assigning them values and colors.
+     */
     @Override
-    public void resetDeck() {
-        log.info("Resetting Deck");
+    public void initialize() {
+        log.info("Initializing Deck");
         Color[] colors = Color.values();
         Value[] values = Value.values();
-        cardsInDeck = 0;
 
         for (Color color : colors) {
             for (int j = 0; j < values.length; j++) {
-                cards.add(new PokerCard(color, Value.getValue(j)));
-                cardsInDeck++;
+                liveCards.add(new PokerCard(color, Value.getValue(j)));
             }
         }
     }
 
+    /**
+     * Reset the deck by making all the cards live again.
+     * The order is not important at this point.
+     */
     @Override
-    public void shuffleDeck() {
-        log.info("Shuffling Deck");
-        if (cardsInDeck < 52) {
-            // TODO: move this to logging
-            System.out.println("LOG: Shuffling incomplete deck. This is ok in some cases of misdeal. Otherwise: error");
-            // let's exit for now
-            return;
-        }
-        Collections.shuffle(cards, new Random());
+    public void reset() {
+        log.info("Resetting Deck");
+        liveCards.addAll(burntCards);
+        burntCards.clear();
+        liveCards.addAll(dealtCards);
+        dealtCards.clear();
     }
 
+    /**
+     * Shuffle only the {@link #liveCards}.
+     * If the live cards are incomplete, i.e. size of {@link #liveCards} is less than {@link #CARDS_IN_STARTING_DECK},
+     * it should be verified that the shuffle is being done because of a misdeal.
+     */
+    @Override
+    public void shuffle() {
+        log.info("Shuffling Deck");
+        if (liveCards.size() < 52) {
+            log.warn("Shuffling incomplete deck. This is only acceptable in case of misdeal.");
+        }
+        Collections.shuffle(liveCards, new Random());
+    }
+
+
+    /**
+     * Draw next card from the deck.
+     * The retrieval is <b>destructive</b>, i.e. the card will be removed from the live collection and added
+     * to the dealt collection.
+     * The Deck is shuffled in advanced, meaning the draw is not random.
+     *
+     * @return a Card from the deck.
+     */
     @Override
     public Card drawCard() {
-        log.info("Drawing one card from Deck");
+        log.info("Drawing card");
+        return null;
+    }
+
+    public Card burnCard() {
+        log.info("Burning card");
         return null;
     }
 
     @Override
     public String toString() {
-        return "PokerDeck {" + System.lineSeparator() +
-                "\tcardsInDeck = " + cardsInDeck + System.lineSeparator() +
-                "\tcards = " +
-                cards.stream().reduce("", (partialResult, card) -> partialResult + "\n" + card.toString(), String::concat)
-                + System.lineSeparator() +
-                '}';
+        return deckToPrettyString(this);
     }
 
-    public List<Card> getCards() {
-        log.info("Getting cards from Deck");
-        return cards;
+    /**
+     * Get all the live existing cards in the deck, i.e. the cards that haven't been dealt or burned.
+     * The retrieval is <b>not destructive</b>.
+     *
+     * @return live cards
+     */
+    List<Card> getLiveCards() {
+        return null;
+    }
+
+    /**
+     * Get all dealt cards in the deck.
+     * The retrieval is <b>not destructive</b>.
+     *
+     * @return all dealt cards
+     */
+    List<Card> getDealtCards() {
+        return null;
+    }
+
+    /**
+     * Get all the burnt cards, i.e. cards not live and not dealt.
+     * The retrieval is <b>not destructive</b>.
+     *
+     * @return all burnt cards
+     */
+    List<Card> getBurntCards() {
+        return null;
+    }
+
+    /**
+     * Get all the existing cards in the deck:
+     * <ul>
+     *     <li>Live cards</li>
+     *     <li>Dealt cards</li>
+     *     <li>Burned cards</li>
+     * </ul>
+     * The retrieval is <b>not destructive</b>.
+     *
+     * @return all the existing cards in the deck.
+     */
+    public List<Card> getAllCards() {
+        log.info("Getting all cards from Deck");
+        return Stream.of(liveCards, dealtCards, burntCards)
+                .flatMap(Collection::parallelStream)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isShuffleBlocked() {
+        return shuffleBlocked;
+    }
+
+    public void setShuffleBlocked(boolean shuffleBlock) {
+        this.shuffleBlocked = shuffleBlock;
     }
 }
