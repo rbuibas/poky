0c9ba2ff0e3fe0887d3065149c0dc2079c44f8bf
rbuibas <buibas.raul@gmail.com>
2020-03-30 01:56:28 +0200

    Test implementations

--- src/test/java/com/poky/core/PokerDeckTest.java
+++ src/test/java/com/poky/core/PokerDeckTest.java
@@ -1,41 +1,125 @@
 package com.poky.core;
 
-import com.poky.util.DeckUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testing the poker deck")
 class PokerDeckTest {
 
+    /**
+     * Maximum number of cards in deck.
+     */
+    int CARDS_IN_STARTING_DECK = 52;
+
     private PokerDeck pokerDeck;
-    private DeckUtils deckUtils;
 
     @BeforeEach
     public void setUp() {
         pokerDeck = new PokerDeck();
-        deckUtils = new DeckUtils();
     }
 
     @Test
+    void initialize() {
+        /*
+        THe deck is initialized when the created.
+        In order to test deck initialization one should:
+        - deep clone the initial object
+        - do some actions, like draw, shuffle on the original deck
+        - compare the deck with the deep clone
+        - re-initialize the object and then compare again
+         */
+//        Still considering whether this is a good idea
+    }
+
+    @Test
+    void burnCard() {
+        fail("Not yet implemented.");
+    }
+
+    @Test
+    void reset() {
+        fail("Not yet implemented.");
+    }
+
+    @Test
+    void getLiveCards() {
+        fail("Not yet implemented.");
+    }
+
+    @Test
+    void getDealtCards() {
+        fail("Not yet implemented.");
+    }
+
+    @Test
+    void getBurntCards() {
+        fail("Not yet implemented.");
+    }
+
+    @Test
+    void getAllCards() {
+        fail("Not yet implemented.");
+    }
+
+    @Nested
     @DisplayName("Test deck resetting")
-    void resetDeck() {
+    class DeckResetting {
+        @Test
+        @DisplayName("Test deck resetting")
+        void resetDeck() {
+            Card card = pokerDeck.dealCard();
+        }
     }
 
-    @Test
+    @Nested
     @DisplayName("Test deck shuffling")
-    void shuffleDeck() {
-        List<Card> cardsInInitialDeck = pokerDeck.getCards();
-        System.out.println(deckUtils.deckToPrettyString(pokerDeck));
-        pokerDeck.shuffleDeck();
-        System.out.println(deckUtils.deckToPrettyString(pokerDeck));
-        // compare the deck after it has been shuffled
-        List<Card> testList = new ArrayList<>();
-        List<Card> cardsInDeckAfterShuffling = pokerDeck.getCards();
-        assertEquals(cardsInInitialDeck, cardsInDeckAfterShuffling);
+    class DeckShuffling {
+        @Test
+        @DisplayName("Test deck shuffling")
+        void shuffleDeck() {
+            List<Card> cardsInInitialDeck = new ArrayList<>(pokerDeck.getAllCards());
+            pokerDeck.shuffle();
+            List<Card> cardsInDeckAfterShuffling = new ArrayList<>(pokerDeck.getAllCards());
+            assertNotEquals(cardsInInitialDeck, cardsInDeckAfterShuffling);
+        }
+    }
+
+    @Nested
+    @DisplayName("Test card dealing")
+    class CardDealing {
+        @Test
+        void dealOneCard() {
+            assertEquals(CARDS_IN_STARTING_DECK, pokerDeck.getLiveCards().size());
+            pokerDeck.dealCard();
+            assertEquals(CARDS_IN_STARTING_DECK - 1, pokerDeck.getLiveCards().size());
+            assertEquals(1, pokerDeck.getDealtCards().size());
+        }
+
+        @Test
+        void dealThreeCards() {
+            assertEquals(CARDS_IN_STARTING_DECK, pokerDeck.getLiveCards().size());
+            for (int i = 0; i < 3; i++) {
+                pokerDeck.dealCard();
+            }
+            assertEquals(CARDS_IN_STARTING_DECK - 3, pokerDeck.getLiveCards().size());
+            assertEquals(3, pokerDeck.getDealtCards().size());
+        }
+
+        @Test
+        void dealMoreCardsThanAvailable() {
+            assertEquals(CARDS_IN_STARTING_DECK, pokerDeck.getLiveCards().size());
+            for (int i = 0; i < CARDS_IN_STARTING_DECK + 1; i++) {
+                pokerDeck.dealCard();
+            }
+            assertEquals(CARDS_IN_STARTING_DECK - 3, pokerDeck.getLiveCards().size());
+            assertEquals(3, pokerDeck.getDealtCards().size());
+        }
     }
 }
