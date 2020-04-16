3b1d70e1e0271fddbacf2f9ea61957b4c9b17dcf
rbuibas <buibas.raul@gmail.com>
2020-04-16 17:32:29 +0200

    Hand strength calculations implementation

--- src/main/java/com/poky/game/HandCalculator.java
+++ src/main/java/com/poky/game/HandCalculator.java
@@ -10,7 +10,17 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class HandCalculator {
     private static final int FIVE_CARDS = 5;
+    private static final int FOUR_CARDS = 4;
+    private static final int THREE_CARDS = 3;
+    private static final int TWO_CARDS = 2;
 
+    /**
+     * Check if flush can be put together from the cards provided in the list.<br/>
+     * If the number of cards is below {@link #FIVE_CARDS}, false is automatically returned.<br/>
+     *
+     * @param sortedCards list of <b>sorted</b> cards
+     * @return true if flush is found, false otherwise
+     */
     public boolean checkFlush(List<Card> sortedCards) {
         if (sortedCards.size() < FIVE_CARDS) return false;
         Map<PokerCardProperties.Color, List<Card>> mapByColors =
@@ -20,6 +30,80 @@ public class HandCalculator {
                 .anyMatch(colors -> colors.size() >= FIVE_CARDS);
     }
 
+    /**
+     * Check if four same value cards exist in the list of provided cards.<br/>
+     * If the number of cards is below {@link #FOUR_CARDS}, false is automatically returned.<br/>
+     *
+     * @param sortedCards list of <b>sorted</b> cards
+     * @return true if four cards of the same value is found, false otherwise
+     */
+    public boolean checkFourOfKind(List<Card> sortedCards) {
+        if (sortedCards.size() < FOUR_CARDS) return false;
+        Map<Integer, List<Card>> mapByValue =
+                sortedCards.stream().collect(groupingBy(Card::getValueOrdinal));
+        return mapByValue.values()
+                .stream()
+                .anyMatch(values -> values.size() >= FOUR_CARDS);
+    }
+
+    /**
+     * Check if three same value cards exist in the list of provided cards.<br/>
+     * If the number of cards is below {@link #THREE_CARDS}, false is automatically returned.<br/>
+     *
+     * @param sortedCards list of <b>sorted</b> cards
+     * @return true if four cards of the same value is found, false otherwise
+     */
+    public boolean checkThreeOfKind(List<Card> sortedCards) {
+        if (sortedCards.size() < THREE_CARDS) return false;
+        Map<Integer, List<Card>> mapByValue =
+                sortedCards.stream().collect(groupingBy(Card::getValueOrdinal));
+        return mapByValue.values()
+                .stream()
+                .anyMatch(values -> values.size() >= THREE_CARDS);
+    }
+
+    /**
+     * Check if two pairs of same value cards exist in the list of provided cards.<br/>
+     * If the number of cards is below {@link #THREE_CARDS}, false is automatically returned.<br/>
+     *
+     * @param sortedCards list of <b>sorted</b> cards
+     * @return true if four cards of the same value is found, false otherwise
+     */
+    public boolean checkTwoPair(List<Card> sortedCards) {
+        if (sortedCards.size() < FOUR_CARDS) return false;
+        Map<Integer, List<Card>> mapByValue =
+                sortedCards.stream().collect(groupingBy(Card::getValueOrdinal));
+        return mapByValue.values()
+                .stream()
+                .filter(values -> values.size() == TWO_CARDS)
+                .count() > 1;
+    }
+
+    /**
+     * Check if two pairs of same value cards exist in the list of provided cards.<br/>
+     * If the number of cards is below {@link #THREE_CARDS}, false is automatically returned.<br/>
+     *
+     * @param sortedCards list of <b>sorted</b> cards
+     * @return true if four cards of the same value is found, false otherwise
+     */
+    public boolean checkPair(List<Card> sortedCards) {
+        if (sortedCards.size() < TWO_CARDS) return false;
+        Map<Integer, List<Card>> mapByValue =
+                sortedCards.stream().collect(groupingBy(Card::getValueOrdinal));
+        return mapByValue.values()
+                .stream()
+                .filter(values -> values.size() == TWO_CARDS)
+                .count() == 1;
+    }
+
+    /**
+     * Check if straight can be put together from the cards provided in the list.<br/>
+     * If the number of cards is below 5, false is automatically returned.<br/>
+     * <b>Important</b>: the provided cards must be sorted by value.
+     *
+     * @param sortedCards list of <b>sorted</b> cards
+     * @return true if straight is found, false otherwise
+     */
     public boolean checkStraight(List<Card> sortedCards) {
         if (sortedCards.size() < FIVE_CARDS) return false;
         // let's do the crude way
